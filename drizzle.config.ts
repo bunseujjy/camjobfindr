17983c19307e1985732385e82aae2f1830e75d1d
bunseujjy <bunseueng@gmail.com>
2025-05-31 11:44:53 +0700

    changing drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,18 @@
- import { env } from "@/data/env/server"
-import { defineConfig } from "drizzle-kit"
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
 
 export default defineConfig({
-    out: "./src/drizzle/migrations",
-    schema: "./src/drizzle/schema.ts",
-    dialect: "postgresql",
-    strict: true,
-    verbose: true,
-    dbCredentials: {
-        password: env.DB_PASSWORD,
-        user: env.DB_USER,
-        database: env.DB_NAME,
-        host: env.DB_HOST,
-        ssl: false
-    }
-})
+  out: "./src/drizzle/migrations",
+  schema: "./src/drizzle/schema.ts",
+  dialect: "postgresql",
+  strict: true,
+  verbose: true,
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    port: Number(process.env.DB_PORT ?? 5432),
+    ssl: true, // likely needed in production (like Supabase or Neon)
+  },
+});
